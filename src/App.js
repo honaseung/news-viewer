f0b30746edaf300b26fc66c90d281ef5274360a0
honaseung <lhs5948@gmail.com>
2022-12-27 14:52:35 +0900

    change app by using react router dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Categories from './components/Categories';
 import NewsList from './components/NewsList';
 import { useCallback, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 function App() {
-    const [category, setCategory] = useState('all');
-    const onSelect = useCallback((category) => {
-        setCategory(category);
-    }, []);
     return (
         <div>
-            <Categories category={category} onSelect={onSelect} />
-            <NewsList category={category} />
+            <Routes>
+                <Route index element={<NewsPage />} />
+                <Route path="/:category" element={<NewsPage />} />
+                {/* <Categories category={category} onSelect={onSelect} />
+                <NewsList category={category} /> */}
+            </Routes>
         </div>
     );
 }
